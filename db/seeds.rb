9ba6576e0d83831bb6ad5d4f4f6d773f1323fd39
Justin Force <justin.force@gmail.com>
2012-10-15 22:37:10 -0700

    Fix a couple typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-['Full time', 'Part time', 'Intern during school', 'Internet out of school'].each do |type|
-  Availability.create(description: type)
+['Full time', 'Part time', 'Intern during school', 'Intern during summer'].each do |desc|
+  Availability.find_or_initialize_by_description(desc).save
 end
